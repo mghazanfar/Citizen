2a2cd223dd406b184ce541bf97eb634e060f2a62
Ghazanfar Ali <ghazanfar.ali@blueeast.com>
2017-12-12 19:16:26 +0500

    Resolved routing

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import Login from './img/slide3.jpg';
 import Typography from 'material-ui/Typography';
 import Button from 'material-ui/Button';
 import Owner from './login/Owner';
+import Loginn from './login/login';
 import Employee from './login/Employee';
 import {
  Route,
  NavLink,
+ Link,
  Switch,
  BrowserRouter as Router
 } from 'react-router-dom';
@@ -22,46 +24,50 @@ const styles = {
     backgroundColor: 'rgba(0,0,0,0.8)',
     height: 'inherit',
     },
-    data: {
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      height: 'inherit',
-      flexDirection: 'column',
-    }
-}
-
-
-class App extends Component {
-  render() {
-    return (
-      <Router>
-        <div style={styles.main}>
-          <div style={styles.inner}>
-            <div style={styles.data}>
-            <Typography type="display3" gutterBottom style={{color:'white'}}>
-              LOGIN AS
-            </Typography>
-            <NavLink to="/Owner">
-            <Button raised component="span" style={{backgroundColor:'white', paddingLeft: 17, paddingRight: 17}}>
-            OWNER
-            </Button>
-            </NavLink>
-            <NavLink to="/Employee">
-            <Button raised component="span" style={{backgroundColor:'rgba(0,150,136,1)', marginTop: '1rem', color: 'white'}}>
-            Employee
-            </Button>
-            </NavLink>
-            </div>
-          </div>
-        </div>
-      </Router> 
-<Switch>
-<Route exact path="/Owner" component={Owner}/>
-<Route path="/Employee" component={Employee} />
-</Switch>
-    );
+  data: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 'inherit',
+    flexDirection: 'column',
+  },
+  noUnderline: {
+    textDecoration: 'none',
   }
 }
 
-export default App;
+const Home = () => (
+  <div style={styles.main}>
+    <div style={styles.inner}>
+      <div style={styles.data}>
+      <Typography type="display3" gutterBottom style={{color:'white'}}>
+        LOGIN AS
+      </Typography>
+      <Link to="/Owner" style={styles.noUnderline}>
+      <Button raised component="span" style={{backgroundColor:'white', minWidth: 110}}>
+      OWNER
+      </Button>
+      </Link>
+      <Link to="/Employee" style={styles.noUnderline}>
+      <Button raised component="span" style={{backgroundColor:'rgba(0,150,136,1)', marginTop: '1rem', color: 'white'}}>
+      Employee
+      </Button>
+      </Link>
+      </div>
+    </div>
+  </div>
+)
+
+
+const ModalGallery = () => (
+  <Router>
+    <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/Employee' component={Employee} />
+          <Route path='/Owner' component={Owner} />
+          <Route path='/Login'  component={Loginn}/>
+        </Switch>
+  </Router>
+)
+
+export default ModalGallery
--- src/login/Owner.js
+++ src/login/Owner.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
 import Typography from 'material-ui/Typography';
 import Button from 'material-ui/Button';
+import Owner from '../img/shop4.jpg';
+import Input, { InputLabel, InputAdornment  } from 'material-ui/Input';
+import TextField from 'material-ui/TextField';
+import { FormControl, FormHelperText  } from 'material-ui/Form';
+import IconButton from 'material-ui/IconButton';
+import Visibility from 'material-ui-icons/Visibility';
+import VisibilityOff from 'material-ui-icons/VisibilityOff';
 import {
  Route,
- NavLink,
+ Link,
  Switch,
 } from 'react-router-dom';
 
@@ -11,15 +18,15 @@ const styles = {
   main: {
     position: 'relative',
     height: '100vh',
+    backgroundImage: "url("+Owner+")",
     backgroundSize: 'cover',
     },
   inner: {
-    backgroundColor: 'rgba(0,0,0,0.8)',
+    backgroundColor: 'rgba(255,255,255,0.8)',
     height: 'inherit',
     },
     data: {
       display: 'flex',
-      justifyContent: 'center',
       alignItems: 'center',
       height: 'inherit',
       flexDirection: 'column',
@@ -28,17 +35,61 @@ const styles = {
 
 
 class App extends Component {
+  state = {
+    amount: '',
+    password: '',
+    weight: '',
+    showPassword: false,
+};
+
+handleChange = prop => event => {
+  this.setState({ [prop]: event.target.value });
+};
+
+handleMouseDownPassword = event => {
+  event.preventDefault();
+};
+
+handleClickShowPasssword = () => {
+  this.setState({ showPassword: !this.state.showPassword });
+};
   render() {
     return (
       <div style={styles.main}>
         <div style={styles.inner}>
           <div style={styles.data}>
-          <Typography type="display3" gutterBottom style={{color:'white'}}>
-            LOGIN AS
+          <Typography type="display2" gutterBottom style={{color:'grey', marginTop:'10rem'}}>
+            OWNER
           </Typography>
-          <Button raised component="span" style={{backgroundColor:'white', paddingLeft: 17, paddingRight: 17}}>
-          OWNER
-          </Button>
+          <FormControl style={{minWidth:350, marginTop: '3rem'}}>
+            <InputLabel>
+              Username
+            </InputLabel>
+            <Input/>
+          </FormControl>
+        <FormControl style={{minWidth:350, marginTop: '1rem'}}>
+          <InputLabel htmlFor="password">Password</InputLabel>
+          <Input
+            id="password"
+            type={this.state.showPassword ? 'text' : 'password'}
+            value={this.state.password}
+            onChange={this.handleChange('password')}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  onClick={this.handleClickShowPasssword}
+                  onMouseDown={this.handleMouseDownPassword}
+                >{this.state.showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+          />
+        </FormControl>
+        <Link to='/Login'>
+        <Button raised component="span" style={{backgroundColor:'rgba(0,150,136,1)', marginTop: '3rem', color: 'white'}}>
+        LOGIN
+        </Button>
+        </Link>
           </div>
         </div>
       </div>
